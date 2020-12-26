b7cf8c8d40dd3a09a3caa7cfe4d75f547005d54e
Chuyang Zhou <469107073@qq.com>
2020-12-26 23:39:25 +1100

    little update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "邱宇和沈宇辰是两只猪!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    cout << "邱宇和沈宇辰是两只猪!" << std::endl;
     return 0;
 }
