5110385fbe15d6fc349e2bb3a13714f2293f1a4d
yshe317 <syc54370579@outlook.com>
2020-12-28 20:09:31 +1100

    new k

---
+++ cmake-build-debug/CMakeFiles/RelicHunter_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RelicHunter_autogen"
+  "RelicHunter_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/RelicHunter_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
