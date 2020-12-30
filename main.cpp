295098ef43f114f9dc23df388361c8eeeea9b342
yshe317 <syc54370579@outlook.com>
2020-12-30 22:26:10 +1100

    Loading scene with bugs: picture cannot be shown

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <QApplication>
-#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include "GUI/LoadingScene.h"
+#include <QString>
+#define SCREENWIDTH 1000
+#define SCREENHEIGHT 800
+void init(QGraphicsView * w){
+    w->resize(SCREENWIDTH,SCREENHEIGHT);
+    w->setWindowFlags(Qt::FramelessWindowHint);
+    w->setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    w->show();
 
+}
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QMainWindow w;
-    w.resize(400,600);
-    w.show();
+    QString path = "resources/city.png";
+    LoadingScene scene(&path);
+
+
+    QGraphicsView view(&scene.scene);
+    init(&view);
+    view.show();
     return a.exec();
 }
+
+
