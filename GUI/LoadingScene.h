20aca0d07de431336943b1f7d334489f5209a490
qy0912 <qy434725138@hotmail.com>
2021-01-02 02:13:46 +1100

    merge

--- GUI/LoadingScene.h
+++ GUI/LoadingScene.h
@@ -9,9 +9,13 @@
 #include <QString>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsRectItem>
+#include <QDebug>
+
+
 class LoadingScene{
 public:
     QGraphicsScene scene;
+
     LoadingScene(QString *filenames){
         QPixmap image = QPixmap(*filenames);
         image = image.scaled(1440,900);
@@ -19,6 +23,9 @@ public:
         QGraphicsRectItem *rec = scene.addRect(0, 0, 100, 100);
         rec->setPos(0,0);
         pic->setPos(0,0);
+        //scene.setSceneRect(0,0,1440,900);
+
+        //qDebug()<<scene.height();
     }
 };
 #endif //RELICHUNTER_LOADINGSCENE_H
