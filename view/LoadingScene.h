0856aa52bb25b8210b49392bad07a5ebbdc89034
yshe317 <syc54370579@outlook.com>
2021-01-02 07:04:07 +1100

    wrong

--- view/LoadingScene.h
+++ view/LoadingScene.h
@@ -15,37 +15,43 @@
 #include <vector>
 #include "animation/Animation.h"
 class LoadingScene : BasicScene{
-
+private:
+    Animation* gear;
 
 public:
     LoadingScene(QGraphicsView* view, QString *filenames);
+    void tick(int frameCount);
 };
 
 
 LoadingScene::LoadingScene(QGraphicsView *view, QString *filenames) : BasicScene(view)
 {
     QPixmap image = QPixmap(*filenames);
+    image = image.scaled(view->size().width(),view->size().height());
+    QGraphicsPixmapItem *pic = scene.addPixmap(image);
+
+    QGraphicsRectItem *rec = scene.addRect(0, view->size().height()/4 *3, view->size().width(), view->size().height()/4);
+    rec->setBrush(QBrush(QColor(0,0,0,200)));
+    pic->setPos(0,0);
 
     QString gear_path1 = ":resources/pic/LoadingGear1.png";
     QString gear_path2 = ":resources/pic/LoadingGear2.png";
     std::vector<QString> paths;
     paths.push_back(gear_path1);
     paths.push_back(gear_path2);
-
-    image = image.scaled(view->size().width(),view->size().height());
-    QGraphicsPixmapItem *pic = scene.addPixmap(image);
-
-
-
-    QGraphicsRectItem *rec = scene.addRect(0, view->size().height()/4 *3, view->size().width(), view->size().height()/4);
-    rec->setBrush(QBrush(QColor(0,0,0,200)));
-    pic->setPos(0,0);
+    Animation gear(paths,scene.width()-200,scene.height()-200,&scene);
+    this->gear = &gear;
 
 
 }
 
-void tick(){
+
+void LoadingScene::tick(int frameCount) {
+    //qDebug()<<"this is here";
+    //gear->tick();
 }
+
+
 #endif //RELICHUNTER_LOADINGSCENE_H
 
 
